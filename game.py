4d02bbed6a8e43189535a0e4b8424d8e9b890730
depa-tto <antonio.depatto@studenti.unimi.it>
2024-10-23 16:18:52 +0200

    game_set

--- game.py
+++ game.py
@@ -8,14 +8,6 @@ player inputs, calculating scores, and determining the winner.
 import pandas as pd # type: ignore
 from quiz import QuizGame # type: ignore
 
-<<<<<<< HEAD
 game_set = pd.read_csv('./game_set.csv')
-=======
-game_set = pd.read_csv('./merge_set.csv')
-game_set(['region'], axis=1, inplace=True)
-game_set.drop_duplicates(inplace=True)
-game_set.to_csv("game_set.csv")
-
->>>>>>> dd7dc99c7569ffa8fa3f68f86ac8306836bc5e8c
 game = QuizGame(game_set)
 game.quiz()
